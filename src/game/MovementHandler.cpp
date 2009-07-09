a7187232027180eae03bfc3c1ac76117f8f466cb
Aion <aion@eternal-knights.de>
2009-07-09 16:19:09 +0200

    Reset to last working Revision

--- src/game/MovementHandler.cpp
+++ src/game/MovementHandler.cpp
@@ -295,8 +295,8 @@ void WorldSession::HandleMovementOpcodes( WorldPacket & recv_data )
 
     if(plMover)                                             // nothing is charmed, or player charmed
     {
-        plMover->SetPosition(movementInfo.x, movementInfo.y, movementInfo.z, movementInfo.o);
         plMover->m_movementInfo = movementInfo;
+		plMover->SetPosition(movementInfo.x, movementInfo.y, movementInfo.z, movementInfo.o);
         plMover->UpdateFallInformationIfNeed(movementInfo, recv_data.GetOpcode());
 
         if(plMover->isMovingOrTurning())
