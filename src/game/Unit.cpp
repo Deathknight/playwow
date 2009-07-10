95592b16eabe4bf6f222f6f69356ce4dbe4b749a
jokker <jokker87@hotmail.com>
2009-07-10 22:33:44 +0200

    fixed shaman hex by Cumulative Pack 8146 patch 88

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -729,9 +729,12 @@ uint32 Unit::DealDamage(Unit *pVictim, uint32 damage, CleanDamage const* cleanDa
                 ((Creature*)pVictim)->AI()->AttackedBy(this);
         }
 
-        // polymorphed and other negative transformed cases
-        if(pVictim->getTransForm() && pVictim->hasUnitState(UNIT_STAT_CONFUSED))
-            pVictim->RemoveAurasDueToSpell(pVictim->getTransForm());
+        // polymorphed, hex and other negative transformed cases
+        if(uint32 morphspell = pVictim->getTransForm())
+            if (IsAuraAddedBySpell(SPELL_AURA_MOD_CONFUSE, morphspell))
+                pVictim->RemoveAurasDueToSpell(morphspell);
+            else if (IsAuraAddedBySpell(SPELL_AURA_MOD_PACIFY_SILENCE, morphspell))
+                pVictim->RemoveSpellbyDamageTaken(SPELL_AURA_MOD_PACIFY_SILENCE, damage);
 
         if(damagetype == DIRECT_DAMAGE || damagetype == SPELL_DIRECT_DAMAGE)
         {
