694701630913a3af255522c214edea4a12abf780
unknown <krd44879@web.de>
2009-07-15 09:34:48 +0200

    Fixed Spell: arcane_blast by cumulative pack 8175_1205 patch 145

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -386,6 +386,10 @@ void Spell::EffectSchoolDMG(uint32 effect_idx)
                 {
                     m_caster->CastSpell(m_caster, 36032, true);
                 }
+                else if (m_spellInfo->SchoolMask & SPELL_SCHOOL_MASK_ARCANE)
+                {
+                    m_caster->RemoveAurasDueToSpell(36032);
+                }
                 break;
             }
             case SPELLFAMILY_WARRIOR:
